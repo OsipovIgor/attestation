59dae89f5458f98084fb4ec6cc6a6c8050edaf44
Osipov Igor <osipov.kaluga@gmail.com>
2018-04-22 23:19:21 +0300

    change port

--- server.js
+++ server.js
@@ -31,6 +31,6 @@ router.post("/add_user", (req, res) => {});
 
 app.use("/api", router);
 
-app.listen(3000, function() {
+app.listen(80, function() {
   console.log("Example app listening on port 3000!");
 });

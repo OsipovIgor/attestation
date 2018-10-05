8dcc5ea80a36873c8d3f26e4a09274300b30f55c
osipov <osipov.kaluga@gmail.com>
2018-10-05 23:19:07 +0300

    txt initial data, created converter class

---
+++ scripts/TxtJsonConverter.ts
@@ -0,0 +1,20 @@
+
+/**
+ * Конвертор данных для бд из TXT в JSON
+ *
+ * @class Converter
+ */
+class Converter {
+
+  platforms: string[] = [];
+
+  constructor(platforms: string[]) {
+    this.platforms = platforms;
+  }
+
+  public convert() {
+
+  }
+
+
+}
